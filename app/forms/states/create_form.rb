91808e26797f511bbbb7cba283f86667034afb60
Tiago Amaro <tiagopadrela@gmail.com>
2016-11-27 20:22:29 +0100

    form defaults

--- app/forms/states/create_form.rb
+++ app/forms/states/create_form.rb
@@ -14,21 +14,32 @@ module States
     end
 
     def country_options
-      scope = ZoneMember
-      scope = scope.where(zone_id: self.zone_id) if self.zone_id.present?
-
+      scope = ZoneMember.includes(:country).where(zone_id: self.zone_id)
       scope.all.map do |zm|
         [zm.country.name, zm.id]
       end
     end
 
     def prepopulate!(options = {})
-      super # Calls form + nested populators
+      super # Calls form + nested prepopulators
 
       if options
         self.zone_id    = options[:zone_id]
         self.country_id = options[:country_id]
+      else
+        self.zone_id    = default_zone.id
+        self.country_id = default_country.id
       end
+    end
+
+    private
+
+    def default_zone
+      Zone.first
+    end
+
+    def default_country
+      default_zone.countries.first
     end
   end
 end
